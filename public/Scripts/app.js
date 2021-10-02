52654c92a553dab4b95d97bb98fb0684824ea661
Revathi <revathibabu55@gmail.com>
2021-10-02 21:23:00 +0530

    portfolio website

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+/*IIFE*/
+
+(function(){
+
+    function Start(){
+        console.log("Welcome...");
+    }
+
+    window.addEventListener("load", Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET Project page. */
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
+
+module.exports = router;
